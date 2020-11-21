3c0e08e0b6eb76eecfc9a5532a5215b81653b4db
Camryn <camrynpatterson@yahoo.com>
2020-11-21 13:08:45 -0600

    commit tree 6d8c6166d52817b9164b349c96e28a1f8218f1ea

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  return false if number <= 1
+  return true if number == 2 || number <= 3
+  sqrt_number = (number ** 0.5).ceil
+  number_array = (2..sqrt_number).to_a
+  prime_checker = true
+  number_array.each do |new_number|
+    if prime?(new_number)
+      if number % new_number == 0
+        prime_checker = false
+      end
+    end
+  end
+  return prime_checker
+end 
+
